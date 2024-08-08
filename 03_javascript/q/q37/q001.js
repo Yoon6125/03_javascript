736013691a6d57710e89d8bea589c7ac8fa4c4e4
Heegeun.Yoon <58425997+Yoon6125@users.noreply.github.com>
2024-08-08 17:34:55 +0900

    Add files via upload

---
+++ 03_javascript/q/q37/q001.js
@@ -0,0 +1,25 @@
+var p1 = 1;
+var p2 = 2;
+var p3 = 3;
+var p4 = 4;
+var p5 = 5;
+var p6 = 6;
+
+var r1= Math.floor(Math.random()*45+1);
+var r2= Math.floor(Math.random()*45+1);
+var r3= Math.floor(Math.random()*45+1);
+var r4= Math.floor(Math.random()*45+1);
+var r5= Math.floor(Math.random()*45+1);
+var r6= Math.floor(Math.random()*45+1);
+
+document.write(r1);
+br();
+document.write(r2);
+br();
+document.write(r3);
+br();
+document.write(r4);
+br();
+document.write(r5);
+br();
+document.write(r6);
