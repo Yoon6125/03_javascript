c7005a0abc39956b677edf605ee27381295f4bad
Heegeun.Yoon <58425997+Yoon6125@users.noreply.github.com>
2024-08-09 17:40:31 +0900

    Add files via upload

---
+++ 03_javascript/q/q45/q001.js
@@ -0,0 +1,19 @@
+function Cat(){
+    this.name="";
+    this.age=0;
+    this.color="";
+
+}
+
+
+
+
+var kitten= new Cat();
+
+kitten.name="새끼고양이";
+kitten.age=1;
+
+
+
+document.write(kitten.name);
+document.write(kitten.age);
